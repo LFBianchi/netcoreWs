66274e0f0a39ac8003d884d51703a85ed3e17820
LFBianchi <lfbianchi3632@gmail.com>
2021-03-29 22:18:24 +0100

    29-02-2021ssesion

---
+++ study/chapter02/exercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object listOfTypes = new object[11];
+
+            sbyte a = 0;
+            byte b = 0;
+            short c = 0;
+            ushort d = 0;
+            int e = 0;
+            uint f = 0;
+            long g = 0;
+            ulong h = 0;
+            float i = 0;
+            double j = 0;
+            decimal k = 0M;
+
+
+            foreach(oject type in listOfTypes)
+            {
+                print(sizeof(type));
+            }   
+            sizeof(int);
+        }
+    }
+}
