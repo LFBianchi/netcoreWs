72ae1b0d788e0220ddce17d73b5dee45a376ed77
LFBianchi <lfbianchi3632@gmail.com>
2021-04-11 12:19:35 +0100

    11-04-2021ssesion

---
+++ study/chapter03/CastingConverting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace CastingConverting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b  = a;
+
+            WriteLine($"The variable a ({a}) is a integer that can be safeli casted to a double: {b}.");
+
+            double c = 9.8;
+            int d = (int)c;
+
+            WriteLine($"The variable c ({c}) is a double that can casted to a int but we loose some data: {d}.");
+
+            long e = 10;
+            int f = (int)e;
+
+            WriteLine($"When e is {e}, (int)e is {f}");
+
+            e = long.MaxValue;
+            f = (int)e;
+
+            WriteLine($"When e is {e}, (int)e is {f}");
+
+            e = 5_000_000_000;
+            f = (int)e;
+
+            WriteLine($"When e is {e}, (int)e is {f}");
+            WriteLine("You get the point, you cast you may loose some stuff.");
+        }
+    }
+}
---
+++ study/chapter03/IteractionStatements/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static System.Console;
+
+namespace IteractionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 10) 
+            {
+                WriteLine(i);
+                i += 1;
+            }
+
+            string password = string.Empty;
+
+            int count = 0;
+            bool fail = false;
+
+            do
+            {
+                Write("Enter your password: ");
+                password = ReadLine();
+                count += 1;
+                if (count >= 10) 
+                {
+                    fail = true;
+                    break;
+                }
+            }
+            while (password != "edinoelia");
+
+            if (fail)
+            {
+                WriteLine("Nops, errou.");
+            }
+            else
+            {
+                WriteLine("Ta sabeno legal.");
+            }
+
+            for (int y =0; y <= 10; y += 1)
+            {
+                WriteLine(y);
+            }
+
+            string[] names = {"Adam", "Barry", "Charlie"};
+
+            foreach (string name in names)
+            {
+                WriteLine($"{name} has {name.Length} characters.");
+            }
+        }
+    }
+}
